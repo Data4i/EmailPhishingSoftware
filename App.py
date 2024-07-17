57facdb23cedbd6b46c76dfc37db2b4e8702c066
data4i0 <paulobiahu@gmail.com>
2024-07-17 14:34:55 +0100

    phishing software

---
+++ App.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import pickle
+
+st.set_page_config(page_title='Email Verifier', layout = 'wide', page_icon=':name_badge:')
+left_col, center_col, right_col = st.columns((1,2,1))
+
+st.sidebar.success("Email Verifier")
+
+model_filepath = 'phising_rf_classifier.pkl'
+
+@st.cache_resource
+def get_model(filepath):
+    with open(filepath, 'rb') as f:
+        model = pickle.load(f)
+    return model
+
+model = get_model(model_filepath)
+
+with center_col:
+    
+    st.title("Email Verifier")
+    st.text("Input Your Email here")
+    email = st.text_input(label= 'email',label_visibility="hidden" ,placeholder='Get $20 Free')
+    check_email = st.button("Check")
+    
+    if check_email:
+        prediction = model.predict([email])
+        if prediction == 'Phishing Email':
+            st.success("Beware Spam", icon="🚨")
+        elif prediction == 'Safe Email':
+            st.success("Verified", icon="🔥")
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # EmailPhishingSoftware
-# EmailPhishingSoftware
+
+## A Streamlit App To Tell If An Email is Legit Or Not
+
+* To run python version must be `>3.9` and `<3.10`
+* Then create a virtual environment with `pip -m venv phishing_venv`
+* Then activate the environment with `source phishing_venv/bin/activate`
+* Then run the streamlit app with `streamlit run App.py`
+
+* The link to the dataset is [https://www.kaggle.com/datasets/subhajournal/phishingemails?resource=download]
+
+### Note: These steps are only for Debian based Linux distros like Ubuntu to run on windows or Mac you have to have the specific code for these steps
